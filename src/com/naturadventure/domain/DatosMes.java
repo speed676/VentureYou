f757d84094fd70703bc5c12b19fc07fdf5e8175f
Pau Alegria <komikclasico@hotmail.com>
2015-07-17 01:46:26 +0200

    Para graficas reservas

---
+++ src/com/naturadventure/domain/DatosMes.java
@@ -0,0 +1,19 @@
+package com.naturadventure.domain;
+
+public class DatosMes{
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	int numero;
+	int mes;
+	
+}
